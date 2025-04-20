ea473f13d414ce0df6cfd02149dfd8e42ec34e15
KamyaMudaliar <kamyamudaliar@gmail.com>
2025-04-20 18:06:10 +0530

    configured spring-boot and thymeleaf

---
+++ src/test/java/com/example/budgettracker/AppTest.java
@@ -0,0 +1,15 @@
+package com.example.budgettracker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
